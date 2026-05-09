7407876e88ec627b739adb4fab8c18e522274a21
edusilvaramos <edusilvaramos@hotmail.com>
2026-05-09 13:56:23 +0200

    feat(app): wire provider and PersistGate in App

--- App.js
+++ App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { tabRouteOptions, tabScreenOptions } from './assets/styles/navigationOptions';
+import { persistor, store } from './src/store';
 
 import HomeScreen from './src/screens/HomeScreen';
 import RecordScreen from './src/screens/RecordScreen';
@@ -13,28 +16,32 @@ const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      
-      {/* all screens */}
-      <Tab.Navigator screenOptions={tabScreenOptions}
-      // ux, is more easy to swipe between tabs when the tab bar is in bottom
-      tabBarPosition="bottom">
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={tabRouteOptions.Home}
-        />
-        <Tab.Screen
-          name="Record"
-          component={RecordScreen}
-          options={tabRouteOptions.Record}
-        />
-        <Tab.Screen
-          name="Rave"
-          component={RaveScreen}
-          options={tabRouteOptions.Rave}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          {/* all screens */}
+          <Tab.Navigator screenOptions={tabScreenOptions}
+          // ux, is more easy to swipe between tabs when the tab bar is in bottom
+          tabBarPosition="bottom">
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={tabRouteOptions.Home}
+            />
+            <Tab.Screen
+              name="Record"
+              component={RecordScreen}
+              options={tabRouteOptions.Record}
+            />
+            <Tab.Screen
+              name="Rave"
+              component={RaveScreen}
+              options={tabRouteOptions.Rave}
+            />
+          </Tab.Navigator>
+          <StatusBar style="auto" />
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
